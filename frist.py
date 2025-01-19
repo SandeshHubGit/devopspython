92724f87db140e12faa3c6c7596c0914347dcb1a
unknown <sandeshgaikwad1994@gmail.com>
2025-01-19 17:28:03 +0530

    Added new changes from local machine to check merge confilct

--- frist.py
+++ frist.py
@@ -4,3 +4,10 @@ i = 1
 while i<=10:
     print("sandesh")
     i = i + 1
+    
+i = int(input("enter the number: "))
+
+if (i % 2) ==0:
+    print(" It is even number")
+else:
+    print("it is odd number")
