4cb849dea04928535f303ee855fe0487eb237725
unknown <sandeshgaikwad1994@gmail.com>
2025-02-17 17:55:38 +0530

    After adding jeson format

--- flusk.py
+++ flusk.py
@@ -2,9 +2,13 @@ import flask
 
 app = flask.Flask(__name__)
 
-@app.route("/hello")
+@app.route('/hello')
 def hello():
-    return "Hello world!"
+    return flask.jsonify({"msg" : "Hello world!"})
+
+@app.route('/about', methods=["POST", "PUT", "GET"])
+def about():
+    return flask.jsonify({"namaste":"it is me"})
 
 
 ## how ro run server
