90bb3f50fa74e38862796767b3b3c9e2445a0943
unknown <sandeshgaikwad1994@gmail.com>
2025-02-18 18:55:25 +0530

    tacking backup

--- flusk.py
+++ flusk.py
@@ -2,19 +2,30 @@ import flask
 
 app = flask.Flask(__name__)
 
-@app.route('/hello')
-def hello():
-    return flask.jsonify({"msg" : "Hello world!"})
+# @app.route('/hello')
+# def hello():
+#     return flask.jsonify({"msg" : "Hello world!"})
 
-@app.route('/about', methods=["POST", "PUT", "GET"])
-def about():
-    return flask.jsonify({"namaste":"it is me"})
+# @app.route('/about', methods=["POST", "PUT", "GET"])
+# def about():
+#     return flask.jsonify({"namaste":"it is me"})
 
-@app.route('/userdata', methods=["POST"])
-def take_from_user():
+# @app.route('/userdata', methods=["POST"])
+# def take_from_user():
+#     data = flask.request.get_json()
+#     print(data)
+#     return "take_from_user"
+
+
+@app.route('/userdatatype', methods=["POST"])
+def take_from_user_type():
     data = flask.request.get_json()
     print(data)
-    return "take_from_user"
+    print(type(data))
+    print(data["name"])
+    print(type(data['name']))
+    return data["name"]
+
 ## how ro run server
 
 if __name__ == "__main__":
